cbdb699c63f65022012521309b66f9cccecf43f4
pkj7323 <pkj7323@naver.com>
2024-11-24 23:52:05 +0900

    이번에는 그레이 스케일로(원래는 rgb더해서 3으로 나무면 되는데 인간의 눈은 녹색에 예민)가중치 곱해서 사용

--- game/framebuffer_screen_fs.glsl
+++ game/framebuffer_screen_fs.glsl
@@ -8,5 +8,6 @@ uniform sampler2D screenTexture;
 void main()
 {
     vec3 col = texture(screenTexture, TexCoords).rgb;
-    FragColor = vec4(vec3(1.0-col), 1.0);
+    float average = 0.2126 * col.r + 0.7152 * col.g + 0.0722 * col.b;
+    FragColor = vec4(average,average,average, 1.0);
 } 
